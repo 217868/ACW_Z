1d58c014f864b3ffa02030c3468e1d7b0736b280
student <student@L407-8.helium.com>
2022-11-19 11:50:55 +0100

    Lab2 controller with params

--- lab2/lab_2/src/main/java/awcz/lab_2/MainController.java
+++ lab2/lab_2/src/main/java/awcz/lab_2/MainController.java
@@ -1,13 +1,33 @@
 package awcz.lab_2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class MainController {
 
-    @GetMapping
-    public String HelloWorld() {
-        return "Hello world";
+    @GetMapping()
+    public String Main(@RequestParam("cmd") Optional<String> cmd) {
+        if (cmd == null) return "Hello world";
+        if (cmd.isEmpty()) return "Hello world";
+
+        switch (cmd.get()) {
+            case "time":
+                return executeTime();
+            case "rev":
+                return executeRev();
+            default:
+                return "Hello world";
+        }
+    }
+
+    private String executeTime() {
+        return "time command";
+    }
+
+    private String executeRev() {
+        return "rev command";
     }
 }
