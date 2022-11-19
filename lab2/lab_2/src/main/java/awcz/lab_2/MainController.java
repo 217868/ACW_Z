a0b6405e674d8105a548c182860e71a6739ac867
student <student@L407-8.helium.com>
2022-11-19 12:02:03 +0100

    lab 2 logic done

--- lab2/lab_2/src/main/java/awcz/lab_2/MainController.java
+++ lab2/lab_2/src/main/java/awcz/lab_2/MainController.java
@@ -2,14 +2,15 @@ package awcz.lab_2;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @RestController
 public class MainController {
 
     @GetMapping()
-    public String Main(@RequestParam("cmd") Optional<String> cmd) {
+    public String Main(@RequestParam("cmd") Optional<String> cmd, @RequestParam("str") Optional<String> str) {
         if (cmd == null) return "Hello world";
         if (cmd.isEmpty()) return "Hello world";
 
@@ -17,17 +18,23 @@ public class MainController {
             case "time":
                 return executeTime();
             case "rev":
-                return executeRev();
+                return executeRev(str.get());
             default:
                 return "Hello world";
         }
     }
 
     private String executeTime() {
-        return "time command";
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
     }
 
-    private String executeRev() {
-        return "rev command";
+    private String executeRev(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
     }
 }
