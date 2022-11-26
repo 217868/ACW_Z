a4d30381c32e2e15a5add6005c8439a7da63bc92
217868 <217868@edu.p.lodz.pl>
2022-11-26 12:26:54 +0100

    Lab 3 done

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+from urllib.parse import urlparse, parse_qs
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -9,16 +10,38 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     
     def do_GET(self):
 
-        print(self.path)
+        parsed_url = urlparse(self.path)
+        parsedArgs = parse_qs(parsed_url.query)
+
+        print(parsed_url.path)
         
-        if self.path == '/':
+        if parsed_url.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.end_headers()
+            if parsedArgs.get('str', None):
+                text = parsedArgs.get('str', None)[0]
+                num_of_low = sum(1 for char in text if char.islower())
+                num_of_high = sum(1 for char in text if char.isupper())
+                num_of_digits = sum(1 for char in text if char.isdigit())
+                num_of_special = sum(1 for char in text if char.isalpha())
+
+                json = {
+                    'lowercase': num_of_low,
+                    'uppercase': num_of_high,
+                    'digits': num_of_digits,
+                    'special': num_of_special
+                }
+
+                self.wfile.write(str(json).encode('utf-8'))
+
+
         else:
             super().do_GET()
+
+
+
     
 # --- main ---
 
